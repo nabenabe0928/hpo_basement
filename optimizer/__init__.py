2d45638994ce2e987be0349f91e47ee39bd7e019
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2019-10-18 19:52:49 +0900

    modify the __init__.py for optmizer

--- optimizer/__init__.py
+++ optimizer/__init__.py
@@ -1,8 +1,11 @@
 from optimizer.base_optimizer import BaseOptimizer
 from optimizer.nelder_mead import NelderMead
 from optimizer.random_search import RandomSearch
+from optimizer.gaussian_process import SingleTaskGPBO
+
 
 __all__ = ['BaseOptimizer',
            'NelderMead',
-           'RandomSearch'
+           'RandomSearch',
+           'SingleTaskGPBO'
            ]
