490392ef58a1f5aa1a98ed72bff5cdbfb92cbd2d
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2019-10-22 19:32:58 +0900

    modify main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     requirements, experimental_settings = utils.parse_requirements()
     # experimental_settings["biased_cls"] = []
     hp_utils = utils.HyperparameterUtilities("cnn", experimental_settings=experimental_settings)
-    opt = optimizer.NelderMead(hp_utils, **requirements)
-    # opt = optimizer.SingleTaskTPE(hp_utils, **requirements)
+    # opt = optimizer.NelderMead(hp_utils, **requirements)
+    opt = optimizer.SingleTaskTPE(hp_utils, **requirements)
     opt.optimize()
