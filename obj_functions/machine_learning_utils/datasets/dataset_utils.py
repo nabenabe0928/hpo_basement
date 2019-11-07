ac34f24691fbf922d5e58435edb33b230c0af034
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2019-11-07 18:28:40 +0900

    modified num_workers

--- obj_functions/machine_learning_utils/datasets/dataset_utils.py
+++ obj_functions/machine_learning_utils/datasets/dataset_utils.py
@@ -48,8 +48,8 @@ def get_dataset(experimental_settings):
 
 
 def get_data(train_dataset, test_dataset, batch_size):
-    train_data = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
-    test_data = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
+    train_data = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=5, pin_memory=True)
+    test_data = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=5, pin_memory=True)
 
     return train_data, test_data
 
